4046e264b310bd134f6c957fd478bfd4b61b667b
firebelly <firebelly@gmail.com>
2022-03-16 12:23:38 -0700

    removed some buttons and changed social location

--- frontend/src/components/layout/header/index.tsx
+++ frontend/src/components/layout/header/index.tsx
@@ -1,8 +1,9 @@
 import css from './header.module.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import SocialLink from '../../social-link';
 
 const Header = (): JSX.Element => {
-    return(
+    return (
         <Navbar fixed="top" expand="md" className={css.navBar}>
             <Container>
                 <Navbar.Brand href="#">
@@ -11,11 +12,14 @@ const Header = (): JSX.Element => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className={`justify-content-end ${css.navbarBg}`}>
                     <Nav className={css.navItem}>
-                        <Nav.Link href="/#">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#chart">Chart</Nav.Link>
-                        <Nav.Link href="https://traderjoexyz.com/trade?outputCurrency=0xA171629637C3266b4247551eE67ffdD3f03Fb0b5">Buy</Nav.Link>
-                        <Nav.Link href="#socials">Socials</Nav.Link>
+                        <Nav.Link title='About' href="#about">About</Nav.Link>
+                        <Nav.Link title='DEX Chart' href="#chart">Chart</Nav.Link>
+                        <Nav.Link title='Buy $SIN' href="https://traderjoexyz.com/trade?outputCurrency=0xA171629637C3266b4247551eE67ffdD3f03Fb0b5">Buy</Nav.Link>
+                        <div className={css.socialBar}>
+                            <SocialLink title="discord"></SocialLink>
+                            <SocialLink title="twitter"></SocialLink>
+                            <SocialLink title="medium"></SocialLink>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
